eeb5fe38ef4ac53fac40768235e84074da1addaf
manoahLinks <manoahluka@gmail.com>
2022-11-24 14:16:41 +0100

    2nd build for deployment

--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,13 @@ const express = require('express'),
       app = express(),
       bodyParser = require('body-parser'),
       mongoose = require('mongoose'),
-      clientRoutes = require('./routes/client')
+      clientRoutes = require('./routes/client'),
+      cors          = require('cors')
       
 app.use(express.static('build'))      
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
+app.use(cors())
 
 app.use('/api/clients', clientRoutes )
 
